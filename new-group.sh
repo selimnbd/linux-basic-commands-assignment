c38cdfe0165f08b8a2b386313a66365532cb930e
selimtbd <selim.tbd@gmail.com>
2023-10-08 15:31:28 +0600

    Remove unnecessary text accept command

--- new-group.sh
+++ new-group.sh
@@ -1,6 +1,3 @@
 root@DevOps:/home/devops# groupadd azad
-	
-	view the groups
-
 root@DevOps:/home/devops# cat /etc/group
 
--- replace-line.sh
+++ replace-line.sh
@@ -1,7 +1,3 @@
 selim@DevOps:~$ find . -type f -name "*.in" | xargs sed "s/Crash in it/crash to broken/g" crash.in
-sed: can't read crash.in: No such file or directory
-crash to broken
-crash to broken
-crash to broken
 selim@DevOps:~$ find . -type f -name "*.in" | xargs sed -i "s/Crash in it/crash to broken/g" crash.in
 
